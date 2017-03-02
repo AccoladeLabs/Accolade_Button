4edc61db84e0360c56362d16c6750808059a3f50
Billy Brawner <billybrawner@gmail.com>
2017-03-02 15:56:15 -0600

    Updated installer script to use universal format

--- app/code/local/Accolade/Button/sql/accoladebutton_setup/install-0.1.0.php
+++ app/code/local/Accolade/Button/sql/accoladebutton_setup/install-0.1.0.php
@@ -1,35 +1,174 @@
 <?php
+/**
+ * This file is part of the Accolade Button for Commerce Magento module.
+ * Please see the license in the root of the directory or at the link below.
+ *
+ * PHP Version 5.6
+ *
+ * @category Magento
+ * @package  Accolade_Button
+ * @author   Accolade Partners <info@accolade.fi>
+ * @license  OSL-3.0 https://opensource.org/licenses/OSL-3.0
+ * @link     https://accolade.fi
+ */
 
+/* @var $this Mage_Core_Model_Resource_Setup */
 $installer = $this;
 
 $installer->startSetup();
 
-$installer->run("
-DROP TABLE IF EXISTS {$this->getTable('accolade_button_associations')};
-DROP TABLE IF EXISTS {$this->getTable('accolade_button_keys')};
+$associationTable = $installer->getConnection()
+    ->newTable(getTable('accolade_button_associations'))
+    ->addColumn(
+        'entity_id',
+        Varien_Db_Ddl_Table::TYPE_INTEGER,
+        11,
+        array(
+            'identity' => true,
+            'nullable' => false,
+            'primary'  => true,
+            'unsigned' => true
+        ),
+        'EntityId'
+    )
+    ->addColumn(
+        'customer_id',
+        Varien_Db_Ddl_Table::TYPE_INTEGER,
+        16,
+        array(
+            'nullable' => false,
+            'unsigned' => true
+        ),
+        'CustomerId'
+    )
+    ->addColumn(
+        'button_id',
+        Varien_Db_Ddl_Table::TYPE_VARCHAR,
+        19,
+        array(
+            'nullable' => false,
+        ),
+        'ButtonId'
+    )
+    ->addColumn(
+        'association_id',
+        Varien_Db_Ddl_Table::TYPE_VARCHAR,
+        100,
+        array(
+            'nullable' => false
+        ),
+        'AssociationId'
+    )
+    ->addColumn(
+        'shipping_method',
+        Varien_Db_Ddl_Table::TYPE_VARCHAR,
+        40,
+        array(
+            'nullable' => false,
+            'default'  => 1
+        ),
+        'ShippingMethod'
+    )
+    ->addColumn(
+        'payment_method',
+        Varien_Db_Ddl_Table::TYPE_VARCHAR,
+        40,
+        array(
+            'nullable' => false
+        ),
+        'PaymentMethod'
+    )
+    ->addColumn(
+        'order_method',
+        Varien_Db_Ddl_Table::TYPE_VARCHAR,
+        40,
+        array(
+            'nullable' => false
+        ),
+        'OrderMethod'
+    );
 
-CREATE TABLE {$this->getTable('accolade_button_associations')} (
-    `entity_id` int(11) unsigned NOT NULL AUTO_INCREMENT,  
-    `customer_id` int(16) unsigned NOT NULL,  
-    `button_id` varchar(19) NOT NULL,
-    `association_id` varchar(100) NOT NULL,
-    `shipping_method` varchar(40) NOT NULL DEFAULT 1,  
-    `payment_method` varchar(40) NOT NULL,
-	`order_method` varchar(40) NOT NULL, 
-    PRIMARY KEY (`entity_id`)
-   ) ENGINE=INNODB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+$installer->getConnection()->createTable($associationTable);
 
-CREATE TABLE {$this->getTable('accolade_button_keys')} (
-    `id` int(8) unsigned NOT NULL AUTO_INCREMENT,  
-    `active` int(2) unsigned NOT NULL,  
-    `name` varchar(256) NOT NULL,
-    `api_key` varchar(128) NOT NULL,
-    `prefix` varchar(64) NOT NULL,
-    `scope` varchar(64) NOT NULL,
-    `created` datetime NOT NULL,
-    `expires` datetime NOT NULL,
-    PRIMARY KEY (`id`)
-   ) ENGINE=INNODB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
-");
+$keyTable = $installer->getConnection()
+    ->newTable(getTable('accolade_button_keys'))
+    ->addColumn(
+        'id',
+        Varien_Db_Ddl_Table::TYPE_INTEGER,
+        8,
+        array(
+            'identity' => true,
+            'nullable' => false,
+            'primary'  => true,
+            'unsigned' => true
+        ),
+        'ID'
+    )
+    ->addColumn(
+        'active',
+        Varien_Db_Ddl_Table::TYPE_INTEGER,
+        2,
+        array(
+            'nullable' => false,
+            'unsigned' => true
+        ),
+        'Active'
+    )
+    ->addColumn(
+        'name',
+        Varien_Db_Ddl_Table::TYPE_VARCHAR,
+        256,
+        array(
+            'nullable' => false,
+        ),
+        'Name'
+    )
+    ->addColumn(
+        'api_key',
+        Varien_Db_Ddl_Table::TYPE_VARCHAR,
+        128,
+        array(
+            'nullable' => false
+        ),
+        'APIKey'
+    )
+    ->addColumn(
+        'prefix',
+        Varien_Db_Ddl_Table::TYPE_VARCHAR,
+        64,
+        array(
+            'nullable' => false
+        ),
+        'Prefix'
+    )
+    ->addColumn(
+        'scope',
+        Varien_Db_Ddl_Table::TYPE_VARCHAR,
+        64,
+        array(
+            'nullable' => false
+        ),
+        'Scope'
+    )
+    ->addColumn(
+        'created',
+        Varien_Db_Ddl_Table::TYPE_DATETIMED,
+        null,
+        array(
+            'nullable' => false
+        ),
+        'Created'
+    )
+    ->addColumn(
+        'expires',
+        Varien_Db_Ddl_Table::TYPE_DATETIMED,
+        null,
+        array(
+            'nullable' => false
+        ),
+        'Expires'
+    );
+
+$installer->getConnection()->createTable($keyTable);
 
 $installer->endSetup();
