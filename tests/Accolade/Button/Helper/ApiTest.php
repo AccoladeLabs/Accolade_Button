8097421927575aebf3dbcdbad0fcd16d6c004022
Billy Brawner <billybrawner@gmail.com>
2017-03-03 18:08:43 -0600

    Started adding some tests for the Api helper

---
+++ tests/Accolade/Button/Helper/ApiTest.php
@@ -0,0 +1,68 @@
+<?php
+
+class Accolade_Button_Helper_ApiTest extends PHPUnit_Framework_TestCase
+{
+    protected $helper;
+    protected $dummyKeys;
+
+    public function setUp()
+    {
+        Mage::app();
+        $this->helper = Mage::helper('accolade_button/api');
+
+        // Prepare some dummy models for use in testing, will be destroyed later
+        $this->dummyKeys = array();
+        $keyScopes = array(
+            'admin',
+            'associate',
+            'press',
+            'read',
+            'write'
+        );
+        foreach ($keyScopes as $scope) {
+            $model = Mage::getModel('accolade_button/key')
+                ->addData(array(
+                    'active' => true,
+                    'name' => $scope,
+                    'api_key' => $scope . '-key',
+                    'prefix' => $scope,
+                    'scope' => $scope
+                ))
+                ->save();
+            $this->dummyKeys[$scope] = $model->getId();
+        }
+    }
+
+    public function testGetMerchant()
+    {
+        Mage::getConfig()->saveConfig('accolade/button/merchant_name', 'test-merchant')->reinit();
+        $this->assertEquals('test-merchant', $this->helper->getMerchant(), 'Merchant name');
+    }
+
+    public function testGetApiKey()
+    {
+        Mage::getConfig()->saveConfig('accolade/button/api_key', 'admin-key')->reinit();
+        foreach ($this->dummyKeys as $scope => $id) {
+            $this->assertEquals($scope . '-key', $this->helper->getApiKey($scope, $id), "Get " . $scope . "key");
+        }
+    }
+
+    public function testGetApiUrl()
+    {
+        // First, set the store config so we don't have any surprises
+        Mage::getConfig()->saveConfig('accolade/button/merchant_name', 'test-merchant')->reinit();
+        // This is what we're expecting to get:
+        $expected = 'https://your.bt.tn/serves/test-merchant/v1/';
+        // So let's test!
+        $this->assertEquals($expected, $this->helper->getApiUrl(), 'API URL');
+    }
+
+    public function tearDown()
+    {
+        foreach ($this->dummyKeys as $key) {
+            Mage::getModel('accolade_button/key')
+                ->load($key)
+                ->delete();
+        }
+    }
+}
